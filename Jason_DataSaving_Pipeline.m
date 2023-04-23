06e80849e6faf09506d4c173c83bbe80c9e80fe0
jasonwhj21 <jasonwhj21@gmail.com>
2023-04-22 22:58:55 -0700

    n ew

--- Jason_DataSaving_Pipeline.m
+++ Jason_DataSaving_Pipeline.m
@@ -8,21 +8,33 @@ folders = dir([path,'/','Dal_*']);
 % interactions = readmatrix([path, '/Dal_Solepnosis/Test_Experiment_DalFormica2.annot'], 'FileType','text');
 % save(outputpath,'curves','distances','velocity','-v7.3')
 num_folders = size(folders,1);
-for i = 1:num_folders
+for i = 1:1
+    
     organism = folders(i).name
     org_folder = [folders(i).folder,'/',organism];
-    load([path,'/',organism,'/',organism,'XY']);
-     [rawData,files] = concatinate(org_folder, str);
+    load([org_folder,'/',organism,'XY'])
+%      [rawData,files] = concatinate(org_folder, str);
      [rawDatafilt, rawDatafiltlin] = filtInterp(rawDatafilt, files, 0.7);
-     velocitylin = velocities2(rawDatafilt);
-     curvaturelin = curvature_circles(rawDatafilt);
+     velocity = velocities2(rawDatafilt);
+     curvature = curvature_circles(rawDatafilt);
+    [distances,interactions] = distance(rawDatafilt,0,75);
+    [distances,before_interactions] = distance(rawDatafilt,75,250);
+    [distances,long_dist_interactions] = distance(rawDatafilt,0,250);
+     flex = flexion(rawDatafilt);
+
+     velocitylin = velocities2(rawDatafiltlin);
+     curvaturelin = curvature_circles(rawDatafiltlin);
     [distanceslin,interactionslin] = distance(rawDatafiltlin,0,75);
     [distanceslin,before_interactionslin] = distance(rawDatafiltlin,75,250);
     [distanceslin,long_dist_interactionslin] = distance(rawDatafiltlin,0,250);
      flexlin = flexion(rawDatafiltlin);
 
-    outputpath = [path,'/',organism,'/',organism,'XYLin'];
-    save(outputpath,'rawDatafiltlin','-v7.3');
-    outputpath2 = [path,'/',organism,'/',organism,'statisticslin'];
-    save(outputpath2,'distanceslin','velocitylin','curvaturelin','flexlin','interactionslin','before_interactionslin','long_dist_interactionslin','-v7.3');
+    outputpath = [path,'/',organism,'/',organism,'XY'];
+    save(outputpath,'rawDatafilt','-v7.3');
+    outputpath1 = [path,'/',organism,'/',organism,'XYLin'];
+    save(outputpath1,'rawDatafiltlin','-v7.3');
+    outputpath2 = [path,'/',organism,'/',organism,'statistics'];
+    save(outputpath2,'distances','velocity','curvature','flex','interactions','before_interactions','long_dist_interactions','-v7.3');
+    outputpath3 = [path,'/',organism,'/',organism,'statisticslin'];
+    save(outputpath3,'distanceslin','velocitylin','curvaturelin','flexlin','interactionslin','before_interactionslin','long_dist_interactionslin','-v7.3');
 end
