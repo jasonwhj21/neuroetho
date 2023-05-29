9d914153305268c224fcc103614dcaf4e5bd3b77
jasonwhj21 <jasonwhj21@gmail.com>
2023-05-28 20:06:03 -0700

    Update flex_vs_flee.m

--- flex_vs_flee.m
+++ flex_vs_flee.m
@@ -6,27 +6,28 @@ neither_interactions = ~(flex_interactions | flee_interactions | both_interactio
 set(0,'defaultAxesFontSize',15)
 types = categorical({'Flex','Flee','Both','Neither', 'All'});
 types = reordercats(types, {'Flex','Flee','Both','Neither', 'All'});
-cat_orientations_other = [mean(orientation(flex_interactions),"omitnan"),mean(orientation(flee_interactions),"omitnan"),mean(orientation(both_interactions),"omitnan"),mean(orientation(neither_interactions),"omitnan"),mean(orientation, "omitnan")];
+cat_approach_speeds = [mean(approach_speed(flex_interactions),"omitnan"),mean(approach_speed(flee_interactions),"omitnan"),mean(approach_speed(both_interactions),"omitnan"),mean(approach_speed(neither_interactions),"omitnan"),mean(approach_speed, "omitnan")];
 
-cat_orientations = [cat_orientations_ants;cat_orientations_other]';
-b = bar(types,cat_orientations)
-title("Average Orientation By Type of Bout")
-legend('Ants','Other')
+% cat_approach_speeds = [cat_approach_speeds_ants;cat_approach_speeds_other]';
+b = bar(types,cat_approach_speeds)
+title("Average approach_speed By Type of Bout")
+% legend('Ants','Other')
 
-SEM_other= [std(orientation(flex_interactions),"omitnan")/sqrt(size(orientation(flex_interactions),2)),
-    std(orientation(flee_interactions),"omitnan")/sqrt(size(orientation(flee_interactions),2)),
-    std(orientation(both_interactions),"omitnan")/sqrt(size(orientation(both_interactions),2)),
-    std(orientation(neither_interactions),"omitnan")/sqrt(size(orientation(neither_interactions),2)),
-    std(orientation,"omitnan")/sqrt(size(orientation,2))];
+SEM_other= [std(approach_speed(flex_interactions),"omitnan")/sqrt(size(approach_speed(flex_interactions),2)),
+    std(approach_speed(flee_interactions),"omitnan")/sqrt(size(approach_speed(flee_interactions),2)),
+    std(approach_speed(both_interactions),"omitnan")/sqrt(size(approach_speed(both_interactions),2)),
+    std(approach_speed(neither_interactions),"omitnan")/sqrt(size(approach_speed(neither_interactions),2)),
+    std(approach_speed,"omitnan")/sqrt(size(approach_speed,2))];
 
-[ngroups,nbars] = size(cat_orientations);
+[nbars,ngroups] = size(cat_approach_speeds);
 % Get the x coordinate of the bars
 x = nan(nbars, ngroups);
 for i = 1:nbars
     x(i,:) = b(i).XEndPoints;
 end
 hold on
-model_error = [SEM_ants,SEM_other];
+% model_error = [SEM_ants,SEM_other];
+model_error = [SEM_other]
 % Plot the errorbars
-errorbar(x',cat_orientations,model_error,'k','linestyle','none');
+errorbar(x',cat_approach_speeds,model_error,'k','linestyle','none');
 hold off
