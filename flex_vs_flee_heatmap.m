3d36f3a04a14dbb5fa41b5e4442364072502486c
jasonwhj21 <jasonwhj21@gmail.com>
2023-06-12 19:41:44 -0700

    news

--- flex_vs_flee_heatmap.m
+++ flex_vs_flee_heatmap.m
@@ -7,7 +7,7 @@ flee_interactions = (dal_velocity_after > 20000 & (flex_during >= 1.75 & flex_af
 both_interactions = ((flex_during < 1.75 | flex_after < 1.75) & dal_velocity_after>20000);
 neither_interactions = ~(flex_interactions | flee_interactions | both_interactions);
 
-approach_divs = linspace(0,20000,20);
+approach_divs = linspace(0,40000,20);
 orientation_divs = linspace(min(orientation),max(orientation),20);
 
 labels = ['Flex', 'Flee', 'Both', 'Neither'];
@@ -16,20 +16,23 @@ labels = ['Flex', 'Flee', 'Both', 'Neither'];
 for i = 1:19
     app(i,:) = approach_speed > approach_divs(i) & approach_speed <= approach_divs(i+1);
     ori(i,:) = orientation > orientation_divs(i) & orientation <= orientation_divs(i+1);
-    total = sum(flex_interactions & app(i,:)) + sum(flee_interactions & app(i,:));
-    flex_percentage_app(i) = sum(app(i,:) & flex_interactions)/total;
-    flee_percentage_app(i) = sum(app(i,:) & flee_interactions)/total;
-    both_percentage_app(i) = sum(app(i,:) & both_interactions)/total;
-    neither_percentage_app(i) = sum(app(i,:) & neither_interactions)/total;
-    flexes_by_app(i) = sum(app(i,:) & flex_interactions);
-    flees_by_app(i) = sum(app(i,:) & flee_interactions);
-    boths_by_app(i) = sum(app(i,:) & both_interactions);
-    neithers_by_app(i) = sum(app(i,:) & neither_interactions);
+    total_danger_response = sum(flex_interactions & app(i,:)) + sum(flee_interactions & app(i,:)) + sum(both_interactions & app(i,:));
+    total = sum(app(i,:));
+    flex_percentage_app(i) = sum(ori(i,:) & flex_interactions)/total_danger_response;
+    flee_percentage_app(i) = sum(ori(i,:) & flee_interactions)/total_danger_response;
+    both_percentage_app(i) = sum(ori(i,:) & both_interactions)/total_danger_response;
+    neither_percentage_app(i) = sum(ori(i,:) & neither_interactions)/total;
+    danger_percentage_app(i) = sum(ori(i,:) & ~neither_interactions)/total;
+    flexes_by_app(i) = sum(ori(i,:) & flex_interactions);
+    flees_by_app(i) = sum(ori(i,:) & flee_interactions);
+    boths_by_app(i) = sum(ori(i,:) & both_interactions);
+    neithers_by_app(i) = sum(ori(i,:) & neither_interactions);
+    dangers_by_app(i) = sum(ori(i,:) & ~neither_interactions);
 end
 all_by_app = [flex_percentage_app; flee_percentage_app; both_percentage_app; neither_percentage_app];
-% for j = 1:9
-%     for k = 1:9
-%         all  = sum(app(i,:) & ori(j,:));
+% for j = 1:19
+%     for k = 1:2
+%         all  = sum(app(j,:) & ori(k,:));
 %         flexes_by_app_ori(j,k) = sum(app(j,:) & ori(k,:) & flex_interactions);
 %         flees_by_app_ori(j,k) = sum(app(j,:) & ori(k,:) & flee_interactions);
 %         bothes_by_app_ori(j,k) = sum(app(j,:) & ori(k,:) & both_interactions);
@@ -40,4 +43,11 @@ all_by_app = [flex_percentage_app; flee_percentage_app; both_percentage_app; nei
 % end
 % surf(approach_divs(1:9),orientation_divs(1:9),flee_percentage)
 % colorbar
-plot(approach_divs(1:19),flex_percentage_app,approach_divs(1:19),flee_percentage_app)
+% fig = figure('units','inch','position',[0,0,10,10]);
+% plot(orientation_divs(1:19),both_percentage_app)
+% titles = "Percentage of Responses that are Flee or Flex by Approach Speed (Ants)"
+% title(titles)
+% xlabel('Approach Speed of Ant (pixels/minute)')
+% ylabel('Percentage of Danger Responses')
+% %
+%  saveas(gcf,'./Ant Graphs/'+ titles + '.png')
--- length_heatmap.m
+++ length_heatmap.m
@@ -3,17 +3,19 @@ flee_interactions = (dal_velocity_after > 20000 & (flex_during >= 1.75 & flex_af
 both_interactions = ((flex_during < 1.75 | flex_after < 1.75) & dal_velocity_after>20000);
 neither_interactions = ~(flex_interactions | flee_interactions | both_interactions);
 
-approach_divs = linspace(0,40000,20);
+approach_divs = linspace(0,40000,10);
 orientation_divs = linspace(min(orientation),max(orientation),20);
-length_by_app = zeros(1,19);
-length_by_ori = zeros(1,19);
-
-for i = 1:19
+length_by_app = zeros(1,9);
+length_by_ori = zeros(1,9);
+error = zeros(1,9);
+for i = 1:9
     app(i,:) = approach_speed > approach_divs(i) & approach_speed <= approach_divs(i+1);
     ori(i,:) = orientation > orientation_divs(i) & orientation <= orientation_divs(i+1);
     length_by_app(i) = mean(length(app(i,:) & ~neither_interactions));
     length_by_ori(i) = mean(length(ori(i,:)));
-    error(i) = std(length(ori(i,:)))/sqrt(size(length(ori(i,:)),2));
+    count_by_app(i) = sum(app(i,:) & ~neither_interactions);
+    count_by_ori(i) = sum(ori(i,:) & ~neither_interactions); 
+    error(i) = std(length(app(i,:) & ~neither_interactions))/sqrt(size(length(app(i,:) & ~neither_interactions),2));
 end
 all_by_app = [flex_percentage_app; flee_percentage_app; both_percentage_app; neither_percentage_app];
 % for j = 1:9
@@ -29,7 +31,11 @@ all_by_app = [flex_percentage_app; flee_percentage_app; both_percentage_app; nei
 % end
 % surf(approach_divs(1:9),orientation_divs(1:9),flee_percentage)
 % colorbar
-errorbar(orientation_divs(1:19),length_by_ori,error)
+subplot(1,2,1)
+errorbar(approach_divs(1:9),length_by_app,error)
 titles = "Length of Interactions by Orientation with Danger Response (Ants)"
 title(titles)
+subplot(1,2,2)
+bar(count_by_app)
+xticks(approach_divs)
 %saveas(gcf,'./Ant Graphs/'+ titles + '.png')
