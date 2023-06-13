3d375296023cc8bb94f8d428c8b396d59929ca80
jasonwhj21 <jasonwhj21@gmail.com>
2023-06-13 11:27:45 -0700

    Update length_heatmap.m

--- length_heatmap.m
+++ length_heatmap.m
@@ -1,4 +1,4 @@
-flex_interactions = ((flex_during < 1.75 | flex_after < 1.75) & dal_velocity_after <= 20000);
+   flex_interactions = ((flex_during < 1.75 | flex_after < 1.75) & dal_velocity_after <= 20000);
 flee_interactions = (dal_velocity_after > 20000 & (flex_during >= 1.75 & flex_after >= 1.75));
 both_interactions = ((flex_during < 1.75 | flex_after < 1.75) & dal_velocity_after>20000);
 neither_interactions = ~(flex_interactions | flee_interactions | both_interactions);
